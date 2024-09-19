9f9a6f828585702a1dfd09edee01fdfd21b89d92
Arihant Sharma <71224541+ArihantSharma@users.noreply.github.com>
2024-09-19 21:41:27 +0530

    Update link_generator.py

--- plugins/link_generator.py
+++ plugins/link_generator.py
@@ -1,7 +1,7 @@
 #(©)Codexbotz
 
-from pyrofork import Client, filters
-from pyrofork.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
+from pyrogram import Client, filters
+from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
 from bot import Bot
 from config import ADMINS
 from helper_func import encode, get_message_id
