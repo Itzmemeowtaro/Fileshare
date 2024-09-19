c17ad84f43b333f603d7707df59b99f1f22386a3
Arihant Sharma <71224541+ArihantSharma@users.noreply.github.com>
2024-09-19 21:07:13 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -1,9 +1,9 @@
 #(©)Codexbotz
 
-from pyrogram import __version__
+from pyrofork import __version__
 from bot import Bot
 from config import OWNER_ID
-from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
+from pyrofork.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
 
 @Bot.on_callback_query()
 async def cb_handler(client: Bot, query: CallbackQuery):
