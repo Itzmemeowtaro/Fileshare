a2b2704656519f7b154d9f15516fc99e15e5a7b2
Arihant Sharma <71224541+ArihantSharma@users.noreply.github.com>
2024-09-20 01:23:43 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -62,7 +62,8 @@ async def start_command(client: Client, message: Message):
         yugen_msgs = []  # List to keep track of sent messages
 
         for msg in messages:
-            caption = CUSTOM_CAPTION.format(previouscaption=msg.caption.html if msg.caption else msg.document.file_name)
+            caption = (CUSTOM_CAPTION.format(previouscaption=msg.caption.html if msg.caption else msg.document.file_name) if bool(CUSTOM_CAPTION) and bool(msg.document)
+                       else ("" if not msg.caption else msg.caption.html))
 
 
             reply_markup = msg.reply_markup if DISABLE_CHANNEL_BUTTON else None
