43ee4af591a3f69b507f06401db92b610cafc36c
Arihant Sharma <71224541+ArihantSharma@users.noreply.github.com>
2024-09-21 19:10:48 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -134,17 +134,17 @@ async def not_joined(client: Client, message: Message):
     except IndexError:
         pass
 
-    await message.reply(
-        text=FORCE_MSG.format(
-            first=message.from_user.first_name,
-            last=message.from_user.last_name,
-            username=None if not message.from_user.username else '@' + message.from_user.username,
-            mention=message.from_user.mention,
-            id=message.from_user.id
-        ),
-        reply_markup=InlineKeyboardMarkup(buttons),
-        quote=True,
-        disable_web_page_preview=True
+    await client.send_photo(
+            chat_id=message.chat.id,
+            photo="https://envs.sh/0QR.jpg",
+            caption=FORCE_MSG.format(
+                first=message.from_user.first_name,
+                last=message.from_user.last_name,
+                username=None if not message.from_user.username else '@' + message.from_user.username,
+                mention=message.from_user.mention,
+                id=message.from_user.id
+            ),
+            reply_markup=InlineKeyboardMarkup(buttons)
     )
 
 @Bot.on_message(filters.command('users') & filters.private & filters.user(ADMINS))
