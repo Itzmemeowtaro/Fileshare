559c120374b5a4a3a665fa947a127f012af814be
Arihant Sharma <71224541+ArihantSharma@users.noreply.github.com>
2024-09-21 08:25:17 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -140,8 +140,9 @@ async def start_command(client: Client, message: Message):
                 ]
             ]
         )
-        await message.reply_text(
-            text=START_MSG.format(
+        await client.send_photo(
+            photo="https://envs.sh/qum.jpg",
+            caption=START_MSG.format(
                 first=message.from_user.first_name,
                 last=message.from_user.last_name,
                 username=None if not message.from_user.username else '@' + message.from_user.username,
