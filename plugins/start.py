3c8bfbadd62ddbb7c13763cda521f6fff4aeb59a
Arihant Sharma <71224541+ArihantSharma@users.noreply.github.com>
2024-09-21 08:53:12 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -141,6 +141,7 @@ async def start_command(client: Client, message: Message):
             ]
         )
         await bot.send_photo(
+            chat_id=message.chat.id,
             photo="https://envs.sh/qum.jpg",
             caption=START_MSG.format(
                 first=message.from_user.first_name,
@@ -148,7 +149,8 @@ async def start_command(client: Client, message: Message):
                 username=None if not message.from_user.username else '@' + message.from_user.username,
                 mention=message.from_user.mention,
                 id=message.from_user.id
-            )
+            ),
+            reply_markup = reply_markup
         )
         return
         
