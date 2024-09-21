2bccff3bdecf29dac14cb1593e3069434eb97bb4
Arihant Sharma <71224541+ArihantSharma@users.noreply.github.com>
2024-09-21 08:57:52 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -140,7 +140,7 @@ async def start_command(client: Client, message: Message):
                 ]
             ]
         )
-        await bot.send_photo(
+        await client.send_photo(
             chat_id=message.chat.id,
             photo="https://envs.sh/qum.jpg",
             caption=START_MSG.format(
