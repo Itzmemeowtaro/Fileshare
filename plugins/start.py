a95dbacb62ae785c101e6903182b51c5ae3dd62a
VØAT <71224541+voatxm@users.noreply.github.com>
2024-09-22 21:06:19 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -100,19 +100,19 @@ async def start_command(client: Client, message: Message):
                 ]
             ]
         )
-        await client.send_photo(
-            chat_id=message.chat.id,
-            photo="image/start.jpg",
-            caption=START_MSG.format(
-                first=message.from_user.first_name,
-                last=message.from_user.last_name,
-                username=None if not message.from_user.username else '@' + message.from_user.username,
-                mention=message.from_user.mention,
-                id=message.from_user.id
+        await message.reply_text(
+            text = START_MSG.format(
+                first = message.from_user.first_name,
+                last = message.from_user.last_name,
+                username = None if not message.from_user.username else '@' + message.from_user.username,
+                mention = message.from_user.mention,
+                id = message.from_user.id
             ),
-            reply_markup=reply_markup
+            reply_markup = reply_markup,
+            disable_web_page_preview = True,
+            quote = True
         )
-        return
+        return 
 
 @Bot.on_message(filters.command('start') & filters.private)
 async def not_joined(client: Client, message: Message):
@@ -134,17 +134,17 @@ async def not_joined(client: Client, message: Message):
     except IndexError:
         pass
 
-    await client.send_photo(
-            chat_id=message.chat.id,
-            photo="image/start.jpg",
-            caption=FORCE_MSG.format(
-                first=message.from_user.first_name,
-                last=message.from_user.last_name,
-                username=None if not message.from_user.username else '@' + message.from_user.username,
-                mention=message.from_user.mention,
-                id=message.from_user.id
+    await message.reply(
+        text = FORCE_MSG.format(
+                first = message.from_user.first_name,
+                last = message.from_user.last_name,
+                username = None if not message.from_user.username else '@' + message.from_user.username,
+                mention = message.from_user.mention,
+                id = message.from_user.id
             ),
-            reply_markup=InlineKeyboardMarkup(buttons)
+        reply_markup = InlineKeyboardMarkup(buttons),
+        quote = True,
+        disable_web_page_preview = True
     )
 
 @Bot.on_message(filters.command('users') & filters.private & filters.user(ADMINS))
